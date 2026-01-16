8272052b3ad0e8c5347314dd5b06dfa2559fdd5d
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-01-16 09:27:24 +0100

    Subo el Ejercicio 2

---
+++ src/main/kotlin/ejercicio2/Vehiculo.kt
@@ -0,0 +1,8 @@
+package ejercicio2
+
+class Vehiculo(private val marca: String, private val modelo: String, private var kilometraje: Double = 0.0) {
+    fun registrarViaje(kilometros: Double) {
+        kilometraje += kilometros
+    }
+    fun detalles(): String {return "Marca >> $marca | Modelo >> $modelo | Kilometraje >> $kilometraje"}
+}
